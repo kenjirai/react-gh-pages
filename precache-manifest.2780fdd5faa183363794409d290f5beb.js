5449349b4ec88b4a758eca12f9ca05b69119cb6e
kenjirai <kenjirai@tuta.io>
2019-02-05 17:16:48 +0545

    Updates

---
+++ precache-manifest.2780fdd5faa183363794409d290f5beb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "a83438fa2e5d3065d82c",
+    "url": "/react-gh-pages/static/js/main.a83438fa.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-gh-pages/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a83438fa2e5d3065d82c",
+    "url": "/react-gh-pages/static/css/main.26f317df.chunk.css"
+  },
+  {
+    "revision": "a4433b822ca1a7bb68d8fd89fa81d783",
+    "url": "/react-gh-pages/index.html"
+  }
+];
